7028d6b53139ed56cdcce84fa5535c41f6e36c74
Michal Nedzka <michael.nedzka@gmail.com>
2018-09-21 23:12:32 +0200

    Add post reducer, modify post_index for clas based component in order to use componentWillMount method for api call

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
+import connect from "react-redux";
+import { bindActioCreators } from "react-redux";
+import { fetchPosts } from "../actions";
 
-export default () => {
-  return <div>List of blog posts</div>;
-};
+class PostIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts;
+  }
+
+  render() {
+    return <div>List of blog posts</div>;
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActioCreators({ fetchPosts }, dispatch);
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(PostIndex);
