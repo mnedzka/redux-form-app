ec26eaa1df1f3e371b83d518782bcd2305265f91
Michal Nedzka <michael.nedzka@gmail.com>
2018-09-22 23:05:35 +0200

    Add redux-form to posts_new component

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
+import { reduxForm } from "redux-form";
 
-export default class PostsNew extends Component {
+class PostsNew extends Component {
   render() {
-    return <div />;
+    return (
+      <form>
+        <h3>Create a new post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea type="text" className="form-control" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    );
   }
 }
+
+export default reduxForm({
+  form: "PostsNew",
+  fields: ["title", "categories", "content"]
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import PostReducer from "./reducer_post";
+import { reducer as formReducer } from "redux-form";
 
 const rootReducer = combineReducers({
   post: PostReducer
