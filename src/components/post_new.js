00f9fb2b4e0f81d9d18f751e4ff91596c18ee6ae
Michal Nedzka <michael.nedzka@gmail.com>
2018-09-22 23:35:43 +0200

    Add action for posting data from user, next steps wit reduxform

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,24 +1,30 @@
 import React, { Component } from "react";
 import { reduxForm } from "redux-form";
+import { createPost } from "../actions";
 
 class PostsNew extends Component {
   render() {
+    const {
+      fields: { title, categories, content },
+      handleSubmit
+    } = this.props;
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create a new post</h3>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...title} />
+          <div className="text-help">{title.touch ? title.error : ""}</div>
         </div>
 
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...categories} />
         </div>
 
         <div className="form-group">
           <label>Content</label>
-          <textarea type="text" className="form-control" />
+          <textarea type="text" className="form-control" {...content} />
         </div>
 
         <button type="submit" className="btn btn-primary">
@@ -29,7 +35,22 @@ class PostsNew extends Component {
   }
 }
 
-export default reduxForm({
-  form: "PostsNew",
-  fields: ["title", "categories", "content"]
-})(PostsNew);
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a username";
+  }
+
+  return errors;
+}
+
+export default reduxForm(
+  {
+    form: "PostsNew",
+    fields: ["title", "categories", "content"],
+    validate
+  },
+  null,
+  { createPost }
+)(PostsNew);
